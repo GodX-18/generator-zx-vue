18ca2954d61fe1cd8bb0cb9d8c28e01823a2dcd2
GodX <1046529973@qq.com>
2023-05-08 09:34:46 +0800

    项目代码优化

--- generators/app/templates/src/stores/user.ts
+++ generators/app/templates/src/stores/user.ts
@@ -11,13 +11,7 @@ export const useUserStore = defineStore("user", {
         declarebm: "",
         declareid: "",
       },
-      businessName: "",
       businessCode: "",
-      enterpriseInfo: {} as enterpriseInfoModel,
-      applyId: "",
-      submitForm: {} as any,
-      action: "view",
-      isLive: true, // 是否现场施工户
       oldApplyId: "", // 老件ID
     };
   },
@@ -29,39 +23,7 @@ export const useUserStore = defineStore("user", {
       } else {
         return true;
       }
-    },
-    ywType: (state) => {
-      switch (state.businessCode) {
-        case "3205900000000010016200001":
-          return "add"; // 核发
-        case "3205900000000010016200002":
-          return "change"; // 变更
-        case "3205900000000010016200003":
-          return "delay"; // 延期
-        default:
-          return "none";
-      }
-    },
-    //* 变更：基本情况可以编辑，其它不行
-    //* 延期：都不可以编辑
-    //* 核发：都可以编辑
-    //* 查看：都不可以编辑
-    isDisabled(): boolean {
-      const route = useRoute();
-      if (this.action === "view") {
-        return true;
-      } else if (
-        this.ywType === "change" &&
-        route.name !== "step1" &&
-        !this.isOld
-      ) {
-        return true;
-      } else if (this.ywType === "delay" && !this.isOld) {
-        return true;
-      } else {
-        return false;
-      }
-    },
+    }
   },
   persist: {
     storage: sessionStorage,
